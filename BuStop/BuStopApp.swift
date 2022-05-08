554de426f9ef114cb62426aeb54b5f7261c719cf
31lie <31lie.adm@gmail.com>
2022-05-08 13:26:41 +0900

    Initial Commit

---
+++ BuStop/BuStopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuStopApp.swift
+//  BuStop
+//
+//  Created by 이윤서 on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct BuStopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BuStop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuStop
+//
+//  Created by 이윤서 on 2022/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
