f7d3d6f80ce830f5a9de7c54042692b542040d96
최명빈 <71762087+choimyungbin@users.noreply.github.com>
2022-05-30 04:29:41 +0900

    버스 정보 조회

--- BuStop/ContentView.swift
+++ BuStop/ContentView.swift
@@ -12,13 +12,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+                //버튼 클릭시버스정류장 정보 불러온 후 현재위치 n미터 이내 정류장이름을 String배열로 불러옮. 그리고 console에 출력
+        
+                let busstop = BusStopInfo(filePath: "/Users/choemyeongbin/Desktop/input.csv")
+
+                let listOfBusStop: [String] = busstop.FindBusStop(10)
+                Button {
+                    
+                    for i in 0..<listOfBusStop.count{
+                        print("list \(i): ",listOfBusStop[i])
+                    }
+                    
+                } label: {
+                    Text("click")
+                }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
