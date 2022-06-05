964b01a1234168ac14c6f9c88422d73f82865fec
user.name <user.email>
2022-06-06 07:11:36 +0900

    경유하는 버스 반환-2

--- BuStop/ContentView.swift
+++ BuStop/ContentView.swift
@@ -7,36 +7,53 @@
 
 
 // geun check
-
+// bin check
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State var numOfStop: String = ""
     var body: some View {
-//        버튼 클릭시버스정류장 정보 불러온 후 현재위치 n미터 이내 정류장이름을 String배열로 불러옮. 그리고 console에 출력
-        let busstop = BusStopInfo(filePath: "/Users/geun/Desktop/input.csv")
-        return Button {
-            
+        //버튼 클릭시 버스정류장 정보 불러온 후 현재위치 n미터 이내 정류장이름을 String배열로 불러옮. 그리고 console에 출력
 
-            let listOfBusStop: [String]? = busstop.FindBusStop(200)
-            if listOfBusStop == nil {
-                print("위치정보 권한 없음")
-            }
-            else{
-                for i in 0..<listOfBusStop!.count{
-                    print(listOfBusStop![i])
+        let busstop = BusStopInfo(filePath: "/Users/choemyeongbin/Desktop/input.csv")
+        let busstop1 = BusInfo(filePath: "/Users/choemyeongbin/Desktop/input.csv")
+        let listOfBusStop: [String]? = busstop.FindBusStop(100)
+        Button {    // nil처리 수정
+            if let checkListOfBusStop = listOfBusStop{
+                for i in 0..<checkListOfBusStop.count{
+                    print("list \(i): ",checkListOfBusStop[i])
                 }
             }
-
         } label: {
-            Text("click")
+            Text("정류장 출력")
         }
-//        Text("Hello")
+        //출발할 정류장 선택
+        HStack{
+            TextField("정류장 번호 입력", text: $numOfStop)
+                .textFieldStyle(.roundedBorder)
+            
+            Button{
+                let busList : [String]? = busstop1.FindBusInfo(from: listOfBusStop!, at: Int(numOfStop)!)
+                print("선택한 정류장을 경유하는 버스")
+                if let checkBusList = busList{
+                    for i in 0..<checkBusList.count{
+                        print("list \(i):",checkBusList[i])
+                    }
+                }
+            } label: {
+                Text("버스출력")
+            } .buttonStyle(.borderless)
+                .padding()
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
